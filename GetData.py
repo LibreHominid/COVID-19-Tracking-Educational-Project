068c78a34ed133108b1e314eaf9f3e66be5e6916
ddnoirot <53709403+ddnoirot@users.noreply.github.com>
2021-02-07 13:06:55 -0700

    Update GetData.py
    ReadMe update
    array initializations

--- GetData.py
+++ GetData.py
@@ -3,7 +3,7 @@ Summary:
 Collect data recorded regarding the SARS-COV-2 (COVID-19) virus. Open file, select fields for use.
 
 Date Created: 
-(YYYY-MM-DD) 2021-1-3
+(YYYY-MM-DD) 2021-01-03
 
 Authors: 
 Daniel Noirot, Brandon Noirot
@@ -37,9 +37,33 @@ f.close()
 # pulling data for Country: USA
 USA = data['USA']
 
+# Initializations
+
 # initialize array for Daily data: total_cases
 total_cases = np.zeros((len(USA['data']),1))
 
-# For loop going through Daily Data
+#initialize array for Daily data: new_cases
+new_cases = total_cases.copy()
+
+#initialize array for Daily data: new_deaths
+new_deaths = total_cases.copy()
+
+#initialize array for Daily data: total_tests
+total_tests = total_cases.copy()
+
+#initialize array for Daily data: new_tests
+new_tests = total_cases.copy()
+
+#initialize array for Daily data: hosp_patients
+hosp_patients = total_cases.copy()
+
+#initialize array for Daily data: days_since_patient_zero
+day_num = total_cases.copy()
+
+#initialize array for Daily data: date
+date = np.empty(total_cases.shape,dtype=str)
+
+
+# For loop going through Daily Data: total_cases
 for ii,day in enumerate(USA['data']):
     total_cases[ii] = day['total_cases']
